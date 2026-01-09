f3af6c570501c1221d9b3c2048f9f23a61f47f53
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-01-10 02:51:38 +0600

    Add files via upload

---
+++ Semester_2/Basic Data Structures/contest/09-01-2026/D_Worth_of_a_Video.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int s;
+        cin >> s;
+        cout << (long long int)(24*1000*s) << endl;
+    }
+    
+    return 0;
+}
---
+++ Semester_2/Basic Data Structures/contest/09-01-2026/J_Long_Loong.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << "L";
+    while(n--){
+        cout << "o";
+    }
+    cout << "ng\n";
+
+    return 0;
+}
+
+
+
+// f the 9 big categories of problems to begin solving.
+
+// 1
+// Beginner
+// Basic problems for anyone who has just started to program...
+
+// 334 PROBLEMS2
+// Ad-Hoc
+// Simulation Problems, Dates, Games and general Ad-Hoc...
+
+// 853 PROBLEMS3
+// Strings
+// Palindromes, Frequency, Ad-Hoc, LCS, String Manipu
