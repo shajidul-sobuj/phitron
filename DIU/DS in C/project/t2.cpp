e39791c6d49f2ebb985a4a2eb60d0cc2c8c15b57
shajidul-sobuj <sajidislam1405@gmail.com>
2026-03-26 23:56:56 +0600

    1

--- DIU/DS in C/project/t2.cpp
+++ DIU/DS in C/project/t2.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #define sz(x) (int)(x).size()
 #define endl '\n'
 
-set<tuple<int, int, string, string>> st;
+unordered_set<tuple<int, int, string, string>> st;
 
 void n1()
 {
@@ -30,7 +30,7 @@ void n1()
 void n2()
 {
     cout << endl << endl << "Date   ||   Time   || Temperature  ||  Humidity\n";
-    set<tuple<int, int, string, string>> pr = st;
+    unordered_set<tuple<int, int, string, string>> pr = st;
     for (auto [a,b,c,d] : pr)
     {
         cout << c << "   ||   " << d << "   ||   " << a << "   ||   " << b << endl;
