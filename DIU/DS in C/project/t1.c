e2321d3229f1aa333f2cfc6eec563b131ff28f58
shajidul-sobuj <sajidislam1405@gmail.com>
2026-03-26 13:06:06 +0600

    12

---
+++ DIU/DS in C/project/t1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node
+{
+    
+} Node;
+
+int main()
+{
+    while(1)
+    {
+        printf("hi");
+        break;
+    }
+}
