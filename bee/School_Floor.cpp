0d1eafff366b31e4e98ce74ca466e6889696a448
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-04-06 14:52:20 +0600

    Create School_Floor.cpp

---
+++ bee/School_Floor.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+
+#define forr(i,a,b) for(int i=a;i<b;i++)
+#define forb(i,a,b) for(int i=a;i>=b;i--)
+
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+
+void yes() { cout << "YES\n"; }
+void no()  { cout << "NO\n";  }
+
+void boom()
+{
+    int l,c;
+    cin >> l >> c;
+    int ans1 = (l*c)+((l-1)*(c-1));
+    int ans2 = 2*(l-1)+2*(c-1);
+    cout << ans1 << endl <<ans2 << endl;
+
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        boom();
+    }
+
+    return 0;
+}
